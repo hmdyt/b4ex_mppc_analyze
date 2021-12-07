9df8143d5be19e0cda29d78ebf2cfda23d5201e0
hmdyt <konnyaku108@gmail.com>
2021-12-07 17:11:55 +0900

    changed to lop

--- 8x2array/src/run_calibration.py
+++ 8x2array/src/run_calibration.py
@@ -2,5 +2,15 @@ from pyroot_easiroc import calibrationUtils as util
 import ROOT as r
 r.gROOT.SetBatch()
 
-json_path = "8x2array/json/cal_20211206_15_ch4.json"
-util.getCalibrationParams(json_path)
+json_paths = []
+
+for ch in range(4, 12):
+    json_path = "8x2array/json/cal_20211206_14_ch{0}.json".format(ch)
+    json_paths.append(json_path)
+    
+for ch in range(20, 28):
+    json_path = "8x2array/json/cal_20211206_21_ch{0}.json".format(ch)
+    json_paths.append(json_path)
+
+for json_path in json_paths:
+    util.getCalibrationParams(json_path)
