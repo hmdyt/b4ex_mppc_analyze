ce56979c516bdbfd96aa336b3eebc79b0a1b2279
tomoe <t.peanuts0115@gmail.com>
2022-01-21 16:02:20 +0900

    make histgram and fitting

---
+++ 8x8array/src/makehistfit.cc
@@ -0,0 +1,59 @@
+#include "TString.h"
+#include "TChain.h"
+#include "TH1D.h"
+#include "TF1.h"
+#include "TMath.h"
+#include "TFile.h"
+#include "TCanvas.h"
+#include "TTree.h"
+#include <vector>
+#include <map>
+#include <utility>
+
+// defult run009 52.86 V
+
+void makehistfit (Double_t HV = 52.86){
+
+    // HV, ch, fit_min, fit_max
+    std::map<double, std::map<int, std::vector<std::pair<int, int>>>> fit_range = fitRange();
+
+    TString file_name = "/data/hamada/easiroc_data/run009.root";
+    TChain *chain = new TChain("tree");
+    chain->Add(file_name);
+    chain->SetBranchStatus("*", 0);
+    chain->SetBranchStatus("VadcHigh", 1);
+    Int_t VadcHigh[64] = {};
+    chain->SetBranchAddress("VadcHigh", &VadcHigh);
+    Int_t N = chain->GetEntries();
+
+    //define canvas
+    TCanvas *canvas = new TCanvas("canvas", "c", 1920*2, 1080*16);
+    canvas->Divide(4, 16);
+    
+    for(Int_t i = 0; i < 64; i++){
+        Int_t X_MIN = 950;
+        Int_t X_MAX = 3500;
+        Int_t X_BIN = X_MAX - X_MIN;
+        canvas->cd(i+1);
+        TH1D *hist = new TH1D("hist", "ADC", X_BIN, X_MIN, X_MAX);
+
+        for(Int_t j = 0; j < N; j++){
+            // make hist
+            chain->GetEntry(j);
+            hist->Fill(VadcHigh[i]);
+        }
+
+        //　define landau fit
+        Int_t fit_MIN = fit_range[HV][i].at(0).first;
+        Int_t fit_MAX = fit_range[HV][i].at(0).second;
+        TF1 *func = new TF1("func", "landau", fit_MIN, fit_MAX);
+
+        // draw
+        hist->Draw();
+        hist->Fit(func,"R");
+    }
+
+    TString save_file_name = file_name.ReplaceAll(".root", "_fit.png");
+    canvas->SaveAs(save_file_name);
+
+}
