b76784c5cfa99f0162d3860f0c0d07bdd2f94ef5
hmdyt <konnyaku108@gmail.com>
2022-01-23 14:11:20 +0900

    get data from input file

--- 8x8array/src/makePlotOfADCtoHV.cc
+++ 8x8array/src/makePlotOfADCtoHV.cc
@@ -5,6 +5,7 @@
 #include "TCanvas.h"
 #include "TF1.h"
 #include <vector>
+#include <fstream>
 
 
 void makePlotOfADCtoHV(){
@@ -20,11 +21,23 @@ void makePlotOfADCtoHV(){
     Int_t FIT_MIN = 950;
     Int_t FIT_MAX = 3500;
     
+    // get data from input file
+    std::ifstream ifs("./ADC_par_HV_list");
+    Double_t chNo, slope = 0;
+    std::vector<std::vector<std::pair<Double_t, Double_t>>> ADC_par_HV_list(64);
+    while(ifs >> chNo >> slope ){
+        ADC_par_HV_list.at(chNo).push_back(std::make_pair(chNo, slope));
+    }
+
+
     // i = ch
     for (Int_t i = 0; i < fitMPV.size(); i++){
         canvas->cd(i + 1);
         TGraph *g = new TGraph();
+        Double_t ADC_HV_slope  = ADC_par_HV_list.at(i).at(0).second;
+        Double_t y_intercept = 52;
         TF1 *func = new TF1("func", "[0] * x + [1]", FIT_MIN, FIT_MAX); 
+        func->SetParameters(ADC_HV_slope, y_intercept);
         for(Int_t j = 0; j < fitMPV.at(i).size(); j++){
             // y
             Double_t HV = fitMPV.at(i).at(j).first;
@@ -37,7 +50,6 @@ void makePlotOfADCtoHV(){
         }
         g->Draw("AP");
         g->Fit(func, "R");
-        //func->GetParameter(0)
     }
     TString save_filename = "/data/hamada/easiroc_data/plotOfADCtoHV.png";
     canvas->SaveAs(save_filename);
