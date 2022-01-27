0bcd51b6a7d982972f97d87928a8a150ba91f79f
hmdyt <konnyaku108@gmail.com>
2022-01-28 05:43:35 +0900

    add method calc_effeciency

--- library/pyroot_easiroc/pyroot_easiroc/README.md
+++ library/pyroot_easiroc/pyroot_easiroc/README.md
@@ -1,6 +1,36 @@
 # TrackSeeker
 TChainの派生クラス。Easirocで取れた測定データの読み込み, FittingによるThresholdの決定, Effeciencyの見積もりなどをする。
 
+## class variables
+- channel 対応図
+  ![](/docs/images/channel_image.jpeg)
+- OUTER_CHNNELS: Set\[int]
+    - 各ボードについているMPPCの上下4chずつ, 計16chが入っている
+- INNER_CHANNELS: Set\[int]
+    - 各ボードについているMPPCの上下4ch以外のch, 計48ch
+- VERTICAL_GROUP_EAST_BOARD: Tuple[Tuple[int]]
+    - 東ボードの鉛直座標が同じMPPCをtupleに詰めている
+        ```
+        (
+            (0, 4, 8, 12, 16, 20, 24, 28),
+            (1, 5, 9, 13, 17, 21, 25, 29),
+            (2, 6, 10, 14, 18, 22, 26, 30),
+            (3, 7, 11, 15, 19, 23, 27, 31)
+        )
+        ```
+-  VERTICAL_GROUP_WEST_BOARD: Tuple[Tuple[int]]
+    - 西ボードの鉛直座標が同じMPPCをtupleに詰めている
+        ```
+        (
+            (60, 56, 52, 48, 44, 40, 36, 32),
+            (61, 57, 53, 49, 45, 41, 37, 33),
+            (62, 58, 54, 50, 46, 42, 38, 34),
+            (63, 59, 55, 51, 47, 43, 39, 35)
+        )
+        ```
+- VERTICAL_GROUP: Tuple[Tuple[int]]
+    - VERTICAL_GROUP_EAST_BOARD + VERTICAL_GROUP_WEST_BOARD
+
 ## Menber variables
 - \_n_event: int
     - 読み込んだrootfileの中の総エントリー数
@@ -8,7 +38,13 @@ TChainの派生クラス。Easirocで取れた測定データの読み込み, Fi
     - VadcHigh\[64]のヒストグラム
 - \__f_landau: List[TF1]\(64)
     - landau fittingに使用する関数
-
+- \_is_hit: List[np.array(dtype=bool)]
+    - thresholdによって決められた, チャンネルごとのヒット情報
+    - len(\_is_hit) == \_n_event
+    - \_is_hit[i].shape == (64, )
+- \_effeciency: List[float]\(64)
+    - 計算されたeffeciencyが格納される
+    - 計算前はNoneが詰まっている
 
 ## Menber functions
 ### constructor(name, filepath)
@@ -24,3 +60,11 @@ TChainの派生クラス。Easirocで取れた測定データの読み込み, Fi
 
 ### save_hist()
 - 64ch全てのヒストグラムをpngに書き出す
+
+### calc_effeciency(ch_target)
+- ch_targetの検出効率を計算する
+- 計算式
+    ```
+    (検出効率) = (ターゲットchと上下のchが鳴ったイベント数) / (上下のchが鳴ったイベント数)
+    ```
+- 上下16ch以外のchに対応している
--- library/pyroot_easiroc/pyroot_easiroc/TrackSeeker.py
+++ library/pyroot_easiroc/pyroot_easiroc/TrackSeeker.py
@@ -1,3 +1,4 @@
+from traceback import print_tb
 from tqdm import tqdm
 import ROOT as r
 import numpy as np
@@ -6,6 +7,21 @@ from . import calibrationUtils as util
 
 
 class TrackSeeker(r.TChain):
+    OUTER_CHNNELS = set([
+        0, 1, 2, 3, 60, 61, 62, 63,
+        28, 29, 30, 31, 32, 33, 34, 35
+        ])
+    INNER_CHANNELS = set(range(64)) - OUTER_CHNNELS
+    VERTICAL_GROUP_EAST_BOARD = tuple(
+        tuple(j + 4*i for i in range(8))
+        for j in range(4)
+    )
+    VERTICAL_GROUP_WEST_BOARD = tuple(
+        tuple(reversed([32 + j + 4*i for i in range(8)]))
+        for j in range(4)
+    )
+    VERTICAL_GROUP = VERTICAL_GROUP_EAST_BOARD + VERTICAL_GROUP_WEST_BOARD
+
     def __init__(self, name, filepath):
         # recieve args
         super().__init__(name)
@@ -24,6 +40,8 @@ class TrackSeeker(r.TChain):
         self._threshold = [0 for _ in range(64)]
         self._landau_fit_range = [[1200, 2700] for _ in range(64)]
         self._f_landau = [r.TF1("f_landau{}".format(ch), "landau", 0, 4096) for ch in range(64)]
+        self._is_hit = []
+        self._effeciency = [None for _ in range(64)]
 
     def set_threshold(self, ch, adc_th):
         self._threshold[ch] = adc_th
@@ -50,17 +68,54 @@ class TrackSeeker(r.TChain):
         save_name = self._filepath.split('/')[-1].replace(".root", ".png")
         canvas.SaveAs(save_name)
 
-    # WIP
     def determine_hit_by_landau_fit(self):
         tree = r.TTree("tree_bool", "tree_bool")
         is_hit = np.zeros(64, dtype=bool)
         tree.Branch("is_hit", is_hit, "is_hit[64]/O")
 
         self._threshold = np.array(self._threshold)
-        for i_event in tqdm(range(self._n_event)):
+        for i_event in tqdm(range(self._n_event), desc="making is_hit"):
             self.GetEntry(i_event)
             VadcHigh = np.array(self.VadcHigh)
             is_hit = self._threshold < VadcHigh
+            self._is_hit.append(is_hit)
             tree.Fill()
         # TODO: consider hit info saving
         tree.SaveAs("tmp.root")
+
+    def calc_effeciency(self, ch_target):
+        """
+        this method is only used inner channel
+        """
+        if ch_target not in self.INNER_CHANNELS:
+            print("ch {} is not inner scintillator!".format(ch_target))
+            exit()
+        # search upside, downside channel
+        for i, group in enumerate(self.VERTICAL_GROUP):
+            if ch_target in group:
+                vertical_group_index = i
+        for i, ch_candi in enumerate(self.VERTICAL_GROUP[vertical_group_index]):
+            if ch_target == ch_candi:
+                ch_down = self.VERTICAL_GROUP[vertical_group_index][i-1]
+                ch_up = self.VERTICAL_GROUP[vertical_group_index][i+1]
+        print("target channel of effeciency calculation is {}, up{}, down{}".format(ch_target, ch_up, ch_down))
+        # calc effeciency
+        ok = 0
+        ng = 0
+        for i_event in tqdm(range(self._n_event), desc="calc eff ch{}".format(ch_target)):
+            if self._is_hit[i_event][ch_up] and self._is_hit[i_event][ch_down]:
+                if self._is_hit[i_event][ch_target]:
+                    ok += 1
+                else:
+                    ng += 1
+            else:
+                continue
+        if (ok+ng) == 0:
+            print("There is no event to calc effeciency ch {}".format(ch_target))
+        else:
+            self._effeciency[ch_target] = ok / (ok + ng)
+            print("detectation effeciency of ch {} is {:.5f}%".format(
+                ch_target,
+                100*self._effeciency[ch_target]
+            ))
+            
