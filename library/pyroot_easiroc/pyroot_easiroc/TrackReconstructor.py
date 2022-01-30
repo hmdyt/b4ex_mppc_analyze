fb757b7c09958862f011ab480702fab034ca5521
HiromitsuYasu <1843105s@stu.kobe-u.ac.jp>
2022-01-30 12:02:55 +0900

    begin to make from now

---
+++ library/pyroot_easiroc/pyroot_easiroc/TrackReconstructor.py
@@ -0,0 +1,18 @@
+import numpy as np
+import ROOT as r
+import sys
+import os
+import plotly.graph_objects as go
+from make_rhombus import Rhombus
+from plotly.subplots import make_subplots
+r.gROOT.SetBatch()
+
+class TrackReconstructor: 
+    # ヒット情報が入ったrootfileのパス
+	def __init__():
+    #上から順番に見ていく
+	def fit_track(i_event):
+
+    #plotlyで可視化	
+	def show(i_event):
+	
