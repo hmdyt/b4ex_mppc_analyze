45c6dc4bf377d4fc1fdd20ef84aff7edec5bea62
hmdyt <konnyaku108@gmail.com>
2022-01-31 00:32:14 +0900

    add memoization

--- main/src/compare_effeciency_by_pedestal.py
+++ main/src/compare_effeciency_by_pedestal.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 import numpy as np
 from tqdm import tqdm
 from pyroot_easiroc.TrackSeeker import TrackSeeker
@@ -15,14 +16,31 @@ class EffeciencyCalc(TrackSeeker):
         self._ref_threshold = [0 for _ in range(64)]
         self._ref_is_hit = []
         self._n_event_calc_effeciency = [0 for _ in range(64)]
+        self._valid_event_list = [[] for _ in range(len(self.VERTICAL_GROUP))]
+        self._valid_event_hit = 5
 
     def set_ref_threshold(self, ch, adc_th):
         self._ref_threshold[ch] = adc_th
+    
+    def require_n_hit(self, n):
+        self._valid_event_hit = n
 
     def fit_by_landau(self, ch):
         super().fit_by_landau(ch)
         for ch in range(64):
             self.set_ref_threshold(ch, self._f_landau[ch].GetParameter(1))
+    
+    def make_valid_event_list(self):
+        for ch in range(64):
+            self.fit_by_landau(ch)
+        self._ref_threshold = np.array(self._ref_threshold)
+        for i_event in tqdm(range(self._n_event), desc="valid event"):
+            self.GetEntry(i_event)
+            VadcHigh = np.array(self.VadcHigh)
+            for vertical_group_index, vertical_chs in enumerate(self.VERTICAL_GROUP):
+                n_vertical_hitted = np.sum(self._ref_threshold[np.array(vertical_chs)] < VadcHigh[np.array(vertical_chs)])
+                if n_vertical_hitted > self._valid_event_list:
+                    self._valid_event_list[vertical_group_index].append(i_event)
 
     def determine_is_hit(self):
         super().determine_hit_by_landau_fit()
@@ -45,7 +63,7 @@ class EffeciencyCalc(TrackSeeker):
         # calc effeciency
         ok = 0
         ng = 0
-        for i_event in tqdm(range(self._n_event), leave=False):
+        for i_event in tqdm(self._valid_event_list[vertical_group_index], leave=False):
             n_vertical_hitted = 0
             for ch_other in target_vertival_group:
                 if ch_target == ch_other:
@@ -67,28 +85,21 @@ class EffeciencyCalc(TrackSeeker):
     def limit_n_event(self, n_event):
         self._n_event = n_event
 
-
-def get_eff(ch, adc_th):
-    ec = EffeciencyCalc("tree", "/data/hamada/easiroc_data/run017.root")
-    for _ch in range(64):
-        ec.fit_by_landau(_ch)
+def get_eff(adc, n_hit):
+    ec = EffeciencyCalc("tree", "run017.root")
+    ec.require_n_hit(n_hit)
     ec.determine_ref_is_hit()
-    for _ch in range(64):
-        ec.set_threshold(_ch, adc_th)
+    ec.make_valid_event_list()
+    for ch in range(64): ec.set_threshold(ch, adc)
     ec.determine_is_hit()
-    ec.calc_effeciency(ch)
-    return ec._effeciency[ch]
-
-
+    for ch in range(64): ec.calc_effeciency(ch)
+    return ec._effeciency
+    
 if __name__ == "__main__":
-    target_ch = int(sys.argv[1])
-    graph = r.TGraph()
-    graph.SetMarkerStyle(8)
-    graph.SetTitle("ch{};Threshold ADC value; effeciency".format(target_ch))
-    adcs = list(range(900, 1500))
-    for adc in adcs:
-        eff = get_eff(target_ch, adc)
-        graph.SetPoint(graph.GetN(), adc, eff)
-    c = r.TCanvas()
-    graph.Draw("AP")
-    c.SaveAs("adc_eff_ch{}.png".format(target_ch))
+    n_hit = int(sys.argv[1])
+    fout = open("compare_eff_ped_{}hit.txt".format(n_hit), 'w')
+    for adc in tqdm(range(900, 1500, 2), desc="ADC"):
+        res = get_eff(adc, n_hit)
+        fout.write("{} ".format(adc))
+        fout.write(" ".join(map(str, res)) + "\n")
+
