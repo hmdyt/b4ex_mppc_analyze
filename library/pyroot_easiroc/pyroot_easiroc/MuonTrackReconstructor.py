86a35ba729ec29a05362d588d810f519f09f027d
hmdyt <konnyaku108@gmail.com>
2022-02-03 12:44:55 +0900

    add constructor arg

--- library/pyroot_easiroc/pyroot_easiroc/MuonTrackReconstructor.py
+++ library/pyroot_easiroc/pyroot_easiroc/MuonTrackReconstructor.py
@@ -1,25 +1,26 @@
 from .TrackReconstructorBase import TrackReconstructorBase
 import numpy as np
 
+
 class MuonTrackReconstructor(TrackReconstructorBase):
-    def __init__(self,rootfile_path) -> None:
-        super().__init__(rootfile_path)
+    def __init__(self, rootfile_path, threshold_s) -> None:
+        super().__init__(rootfile_path, threshold_s)
         self._hit_array = self._hit_array_gen.get_hit_array()
-    
+
     def hit_muon_straight(self):
-        self.hit_muon = np.empty((0,8,4,4),int)
+        self.hit_muon = np.empty((0, 8, 4, 4), int)
         for k in range(8):
-            #k層めにイベントがなければこのイベントは無視
+            # k層めにイベントがなければこのイベントは無視
             if (self._hit_array[k].any() == 0):
                 break
-            #最後の層を見てイベントがあれば真っ直ぐミューオンが通ったイベントとしてhit_muonに詰める
+            # 最後の層を見てイベントがあれば真っ直ぐミューオンが通ったイベントとしてhit_muonに詰める
             if(k == 7):
                 if(self._hit_array[k].any == 0):
                     break
-                self._hit_array = np.reshape(self._hit_array, (1,8,4,4))
+                self._hit_array = np.reshape(self._hit_array, (1, 8, 4, 4))
                 self.hit_muon = np.append(self.hit_muon, self._hit_array, axis=0)
                 break
-            
+
             for i in range(4):
                 for j in range(4):
                     # 反応したものがあると入る分岐
@@ -33,7 +34,7 @@ class MuonTrackReconstructor(TrackReconstructorBase):
                             if(hit_slice.any() == 0):
                                 break
                             continue
-                        if(j ==0):
+                        if(j == 0):
                             hit_slice = self._hit_array[k+1][i-1:i+2, 0:j+2]
                             if(hit_slice.any() == 0):
                                 break
@@ -41,6 +42,4 @@ class MuonTrackReconstructor(TrackReconstructorBase):
                         else:
                             hit_slice = self._hit_array[k+1][i-1:i+2, j-1:j+2]
                             if(hit_slice.any() == 0):
-                                break  
-    
-                              
+                                break
