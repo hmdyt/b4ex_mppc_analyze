dd2df93498c83bcd5f811ec89df38a71ed6c1f93
HiromitsuYasu <1843105s@stu.kobe-u.ac.jp>
2022-02-07 00:57:56 +0900

    finish dirty code

---
+++ main/src/use_FitPointMuonTrackReconstructor.py
@@ -0,0 +1,23 @@
+from pyroot_easiroc.FitPointMuonTrackReconstructor import FitPointMuonTrackReconstructor
+from tqdm import tqdm
+
+rootfile_path = "/data/hamada/easiroc_data/run017.root"
+threshold_s = [1200] * 64
+
+fpmtr = FitPointMuonTrackReconstructor(rootfile_path, threshold_s)
+
+# cut event
+fpmtr._pre_cut_threshold_layer()
+fpmtr._select_top_layer_hit_event()
+fpmtr._select_bottom_layer_hit_event()
+fpmtr._select_2hits()
+fpmtr._select_top_layer_continue_hits_event()
+fpmtr._select_continue_2_hits()
+
+i_event = 0
+
+# fit 
+fpmtr._get_fit_point(fpmtr._select_continue_2_hits_index[i_event])
+fpmtr.fiting_func(fpmtr._param, fpmtr._x, fpmtr._y, fpmtr._z)
+fpmtr._get_fit_line_equition(fpmtr._select_continue_2_hits_index[i_event])
+fpmtr.show(fpmtr._select_continue_2_hits_index[i_event])
