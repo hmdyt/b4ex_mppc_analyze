cf298ddc7b86a794c6e5cb81cad815ff1ca4013d
HiromitsuYasu <1843105s@stu.kobe-u.ac.jp>
2022-02-07 14:33:35 +0900

    add select

--- main/src/use_FitPointMuonTrackReconstructor.py
+++ main/src/use_FitPointMuonTrackReconstructor.py
@@ -6,18 +6,20 @@ threshold_s = [1200] * 64
 
 fpmtr = FitPointMuonTrackReconstructor(rootfile_path, threshold_s)
 
+fpmtr = FitPointMuonTrackReconstructor(rootfile_path, threshold_s)
+
 # cut event
 fpmtr._pre_cut_threshold_layer()
 fpmtr._select_top_layer_hit_event()
 fpmtr._select_bottom_layer_hit_event()
+fpmtr._cut_non_2_layer_continue_event()
 fpmtr._select_2hits()
 fpmtr._select_top_layer_continue_hits_event()
 fpmtr._select_continue_2_hits()
 
-i_event = 0
-
 # fit 
-fpmtr._get_fit_point(fpmtr._select_continue_2_hits_index[i_event])
-fpmtr.fiting_func(fpmtr._param, fpmtr._x, fpmtr._y, fpmtr._z)
-fpmtr._get_fit_line_equition(fpmtr._select_continue_2_hits_index[i_event])
-fpmtr.show(fpmtr._select_continue_2_hits_index[i_event])
+for i_event in tqdm(range(len(fpmtr._select_continue_2_hits_index))):
+    #fpmtr._get_fit_point(fpmtr._select_continue_2_hits_index[i_event])
+    #fpmtr.fiting_func(fpmtr._param, fpmtr._x, fpmtr._y, fpmtr._z)
+    #fpmtr._get_fit_line_equition(fpmtr._select_continue_2_hits_index[i_event])
+    fpmtr.show(fpmtr._select_continue_2_hits_index[i_event])
