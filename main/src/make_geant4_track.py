eebb3d0d40852a513fe8b07f052c0539d98aa7d6
hmdyt <konnyaku108@gmail.com>
2022-02-07 16:26:01 +0900

    geant4 data update

--- main/src/make_geant4_track.py
+++ main/src/make_geant4_track.py
@@ -1,18 +1,18 @@
-from pyroot_easiroc.MuonTrackReconstructor import MuonTrackReconstructor
+from pyroot_easiroc.FitPointMuonTrackReconstructor import FitPointMuonTrackReconstructor
 from tqdm import tqdm
 
-rootfile_path = "convertSimulationData.root"
+rootfile_path = "/data/hamada/simulation_data/convertSimulationData_09.root"
 threshold_s = [0.5] * 64
 
-mtr = MuonTrackReconstructor(rootfile_path, threshold_s)
-mtr._pre_cut_threshold_layer()
-mtr._multi_hit()
-mtr._under_layer_limit()
-mtr.hit_muon_straight()
+fpmtr = FitPointMuonTrackReconstructor(rootfile_path, threshold_s)
+fpmtr._pre_cut_threshold_layer()
+fpmtr._select_top_layer_hit_event()
+fpmtr._select_bottom_layer_hit_event()
+fpmtr._cut_non_2_layer_continue_event()
+fpmtr._select_2hits()
+fpmtr._select_top_layer_continue_hits_event()
+fpmtr._select_continue_2_hits()
 
 with open("make_geant4_track.txt", 'w') as f:
-    for i_event in mtr._under_layer_limit_index:
+    for i_event in fpmtr._select_continue_2_hits_index:
         f.write("{}\n".format(i_event))
-
-for i_event in tqdm(range(len(mtr._under_layer_limit_index)), desc="i_event"):
-    mtr.show(mtr._under_layer_limit_index[i_event])
